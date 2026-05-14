16c7e8212d85105a1d39d042323a366604db6548
DESKTOP-R02KDMM\student <nagashima@jec.ac.jp>
2026-05-14 15:41:42 +0900

    敵キャラ1体を追加

--- Source/ECHO/Public/Enemy/AIController/EnemyAIController.h
+++ Source/ECHO/Public/Enemy/AIController/EnemyAIController.h
@@ -9,8 +9,8 @@
 class UAIPerceptionComponent;
 class UAISenseConfig_Sight;
 class UAISenseConfig_Hearing;
-class UBehaviourTree;
-
+class UBehaviorTreeComponent; 
+class UBlackboardComponent;
 
 UCLASS()
 class ECHO_API AEnemyAIController : public AAIController
@@ -21,18 +21,41 @@ public:
 	AEnemyAIController();
 
 protected:
+	//ゲーム開発開始時の初期化関数
 	virtual void BeginPlay() override;
 
+	//Pawnをコントロールする際の初期化関数
 	virtual void OnPossess(APawn* _inPawn) override;
 
+	//ターゲットの知覚が更新されたときに呼び出される関数
 	UFUNCTION()
 	void OnTargetPerceptionUpdated(AActor* _actor, FAIStimulus _stimulus);
 
-private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "AI")
+public:
+	//ターゲットを設定する関数
+	virtual void SetTargetActor(AActor* _actor);
+
+	//ターゲットをクリアする関数
+	virtual void ClearTarget();
+
+protected:
+	//AIの知覚コンポーネントへの参照
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "AI")
 	UAIPerceptionComponent* m_pAIPerceptionComp;
 
+	//視覚の設定への参照
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "AI")
 	UAISenseConfig_Sight* m_pSightConfig;
 
+	//聴覚の設定への参照
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "AI")
 	UAISenseConfig_Hearing* m_pHearingConfig;
+
+	//ビヘイビアツリーコンポーネントへの参照
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "AI")
+	UBehaviorTreeComponent* m_pBehaviorTreeComp;
+
+	//ブラックボードコンポーネントへの参照
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "AI")
+	UBlackboardComponent* m_pBlackboardComp;
 };
